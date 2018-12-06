55d5eaa7d3e94e0e300c4afb2040475675e80c28
Chelsea Kaye Punzalan <35748136+chelseaxkaye@users.noreply.github.com>
2018-12-06 11:53:18 -0800

    Add files via upload

---
+++ Hashtable.java
@@ -0,0 +1,88 @@
+import java.util.*;
+import java.io.*;
+
+public class Hashtable {
+
+	//inner class hashnode
+	public class HashNode {
+		public String key;
+		public String value;
+		public HashNode next;
+		public HashNode(String k, String v) {
+			key = k;
+			value = v;
+			next = null;
+		}
+	}
+
+	private int num_bucks;
+	private ArrayList<HashNode> buckets;
+	private int size;
+
+	public Hashtable() {
+		num_bucks = 20000;
+		size = 0;
+		buckets = new ArrayList();
+		for (int i = 0; i < num_bucks; i++)
+			buckets.add(null);
+	}
+
+	public int getBucket(String k) {
+		int code = Math.abs(k.hashCode());
+		return code % num_bucks;
+	}
+
+	public String get(String k) {
+		int id = getBucket(k);
+		HashNode node = buckets.get(id);
+		while (node != null) {
+			if (node.key.equals(k))
+				return node.value;
+			node = node.next;
+		}
+		return null;
+	}
+
+	public boolean containsKey(String k) {
+		String x = get(k);
+		if (x == null)
+			return false;
+		return true;
+	}
+
+	public void put(String k, String v) {
+		int id = getBucket(k);
+		HashNode node = buckets.get(id);
+		// while (node != null) {
+		// 	if (node.key.equals(k)) {
+		// 		node.value = v;
+		// 		return;
+		// 	}
+		// 	node = node.next;
+		// }
+		HashNode head = new HashNode(k, v);
+		head.next = node;
+		buckets.set(id, head);
+		size++;
+	}
+
+	public String remove(String k) {
+		int id = getBucket(k);
+		HashNode node = buckets.get(id);
+		HashNode prev = null;
+		while (node != null) {
+			if (node.key.equals(k))
+				break;
+			prev = node;
+			node = node.next;
+		}
+		if (node == null)
+			return null;
+		size--;
+		if (prev == null)
+			buckets.set(id, node.next);
+		else
+			prev.next = node.next;
+		return node.value;
+	}
+}
